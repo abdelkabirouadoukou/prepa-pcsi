cf45376e0c7bdcff353e4545f4f1c8771ee35ea2
Abdelkabir Ouadoukou <abdelkabir.ouadoukou@gmail.com>
2025-11-05 21:39:59 +0100

    ✨Add main.py for calculating the surface area of a trapezoid

---
+++ tp/tp1/surface-dun-trapeze/main.py
@@ -0,0 +1,7 @@
+basePetite = float(input("Donne la Base Petite b:"))
+baseGrand = float(input("Donne la Base Grand B:"))
+hauteur = float(input("Donne le Hauteur h:"))
+
+SURFACE = ((baseGrand+basePetite)*hauteur)/2
+
+print(SURFACE)
