f49f2d38baf90297fc4dda7570144b0c1f69d2a0
Abdelkabir Ouadoukou <152230716+abdelkabirouadoukou@users.noreply.github.com>
2026-04-22 14:03:21 +0100

    Implement k-mer functions for sequence analysis

---
+++ Informatique/dl/ex3-dl1.py
@@ -0,0 +1,20 @@
+def k_mer(text,h):
+  dico = {}
+  for i in range(len(text)-k+1):
+    sous_chaine= text[i:i+k]
+    dico[i+1] = sous_chaine
+  return dico
+def estExiste(text, ch):
+  k=len(ch)
+  repertoire = k_mer(text, k)
+  return ch in repertoire.values()
+def decalage(text, ch):
+  indices = []
+  k=len(ch)
+  repertoire = k_mer(text, k)
+  for cle, valeur in repertoire.items():
+    if valeur == ch:
+      indices.append(cle-1)
+  return indices
+t="ACTGTTAGCAAXTAGGWTSHSTGH"
+print(decalage(t,motif(f))
