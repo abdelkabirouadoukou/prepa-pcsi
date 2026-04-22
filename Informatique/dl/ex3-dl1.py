4aca545b90cd44849933d6fc8623faca5d049b0e
Abdelkabir Ouadoukou <152230716+abdelkabirouadoukou@users.noreply.github.com>
2026-04-22 14:05:19 +0100

    Refactor k_mer and estExiste functions for consistency

--- Informatique/dl/ex3-dl1.py
+++ Informatique/dl/ex3-dl1.py
@@ -1,20 +1,23 @@
-def k_mer(text,h):
-  dico = {}
-  for i in range(len(text)-k+1):
-    sous_chaine= text[i:i+k]
-    dico[i+1] = sous_chaine
-  return dico
+def k_mer(text, k):
+    dico = {}
+    for i in range(len(text) - k + 1):
+        sous_chaine= text[i:i+k]
+        dico[i+1] =sous_chaine
+    return dico
+
 def estExiste(text, ch):
-  k=len(ch)
-  repertoire = k_mer(text, k)
-  return ch in repertoire.values()
+    k = len(ch)
+    repertoire =k_mer(text, k)
+    return ch in repertoire.values()
+
 def decalage(text, ch):
-  indices = []
-  k=len(ch)
-  repertoire = k_mer(text, k)
-  for cle, valeur in repertoire.items():
-    if valeur == ch:
-      indices.append(cle-1)
-  return indices
-t="ACTGTTAGCAAXTAGGWTSHSTGH"
-print(decalage(t,motif(f))
+    indices=[]
+    k =len(ch)
+    repertoire = k_mer(text, k)
+    for cle,valeur in repertoire.items():
+        if valeur== ch:
+            indices.append(cle - 1)
+    return indices
+
+t = "ACTGTTAGCAAXTAGGWTSHSTGH"
+print(decalage(t, "TAG"))
