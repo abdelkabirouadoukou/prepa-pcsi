dce07fee5c710d9398d415afc62378c3b8c1131c
Abdelkabir Ouadoukou <abdelkabir.ouadoukou@gmail.com>
2026-06-10 16:35:52 +0100

    🦾feat: Add implicit Euler method for solving differential equations

--- Informatique/TPs-TDs/eq-diff/1-order.py
+++ Informatique/TPs-TDs/eq-diff/1-order.py
@@ -22,16 +22,29 @@ def eulerExplicite(f,a,b,n,y_0):
     
     return T, Y
 
+def eulerExpliciteImplicit(f,a,b,n,y_0):
+    h=(b-a)/n
+    T_imp=subDiviser(a,b,n)
+    Y_imp=np.zeros(len(T_imp))
+    Y_imp[0]=y_0
+    for i in range(0,n):
+        U=Y_imp[i] + h*f(T_imp[i],Y_imp[i])
+        Y_imp[i+1] = Y_imp[i] + h*f(T_imp[i],U)
+    
+    return T_imp, Y_imp
+
 a, b, n, y_0 = 0, 2, 100, 1
 T, Y = eulerExplicite(f, a, b, n, y_0)
+T_imp, Y_imp = eulerExpliciteImplicit(f, a, b, n, y_0)
 
 T_exact = np.linspace(a, b, 200)
 Y_exact = np.exp(-(T_exact**2) / 2)
 
 plt.figure(figsize=(10, 6))
 plt.plot(T, Y, 'b-', label=f"Approximation d'Euler (n={n})", linewidth=2)
+plt.plot(T_imp, Y_imp, 'g-.', label=f"Approximation d'Euler implicite (n={n})", linewidth=2)
 plt.plot(T_exact, Y_exact, 'r--', label="Solution exacte ($e^{-t^2/2}$)", linewidth=1.5)
-
+    
 plt.title("Méthode d'Euler Explicite pour $dy/dt = -yt$", fontsize=14)
 plt.xlabel("t", fontsize=12)
 plt.ylabel("y(t)", fontsize=12)
